7f42632be774ce86c45adbad50bf3151979a64d8
Ofir Kariv <ofir.kariv@bmw28.cs.colman.ac.il>
2018-06-04 00:52:09 +0300

    New UI version

--- Final_Project/Model/SharedModelFB.swift
+++ Final_Project/Model/SharedModelFB.swift
@@ -1,24 +1,100 @@
 //
+
 //  SharedModelFB.swift
+
 //  Final_Project
-//
-//  Created by ofir kariv on 6/3/18.
-//  Copyright © 2018 admin. All rights reserved.
+
 //
 
+//  Created by ofir kariv on 6/3/18.
+
+//  Copyright © 2018 admin. All rights reserved.
+
+//
+
+
+
 import Foundation
+
 import Firebase
+
+
+
 
 
 class SharedModelFB {
     
+    
+    
     let ref : DatabaseReference?
+    
+    
     
     init() {
         
+        
+        
         ref = Database.database().reference()
         
+        
+        
     }
-   
+    
+    
+    
+    func addPlan(pubPlan : SharedPublicPlan){
+        
+        
+        
+        let myRef = ref?.child("Shared").child(pubPlan.id!)
+        
+        
+        
+        myRef?.setValue(pubPlan.toJson())
+        
+    }
+    
+    
+    
+    
+    
+    func getAllPlans (pubPlan : SharedPublicPlan , callback : @escaping ([SharedPublicPlan]?) -> Void) {
+        
+        
+        
+        let myRef = ref?.child("Shared").child(pubPlan.id!)
+        
+        myRef?.observe(.value, with: { (snapshot) in
+            
+            
+            
+            if let values = snapshot.value as? [String : [String : Any]] {
+                
+                var planArr = [SharedPublicPlan]()
+                
+                for planJson in values{
+                    
+                    let sharedPlan = SharedPublicPlan(fromJson: planJson.value)
+                    
+                    planArr.append(sharedPlan)
+                    
+                }
+                
+                callback(planArr)
+                
+            }
+                
+            else{
+                
+                callback(nil)
+                
+            }
+            
+        })
+        
+    }
+    
+    
     
 }
+
--- Final_Project/Model/SharedPublicPlan.swift
+++ Final_Project/Model/SharedPublicPlan.swift
@@ -1,46 +1,136 @@
 //
+
 //  SharedPublicPlan.swift
+
 //  Final_Project
+
 //
+
 //  Created by ofir kariv on 6/3/18.
+
 //  Copyright © 2018 admin. All rights reserved.
+
 //
+
+
 
 import Foundation
 
+
+
 class SharedPublicPlan {
     
+    
+    
     var id : String?
+    
     var plan : Plan?
+    
     var user : User?
+    
     var likesCount : Int?
+    
     var likesUserSet = Set<String>()
+    
+    
     
     init(id : String , plan : Plan? , user : User? , likesCount : Int?, likesSet : Set<String>) {
         
+        
+        
         self.id = NSUUID().uuidString
+        
         self.plan = plan
+        
         self.user = user
+        
         self.likesCount = likesCount
+        
         self.likesUserSet = likesSet
+        
     }
+    
+    
+    
     
     
     init(fromJson : [String : Any]) {
         
+        
+        
         self.id = NSUUID().uuidString
+        
         self.plan = fromJson["plan"] as? Plan
+        
         self.user = fromJson["user"] as? User
+        
         self.likesCount = fromJson["likesCount"] as? Int
+        
         self.likesUserSet = (fromJson["likesSet"] as? Set<String>)!
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    func toJson()-> [String : Any] {
+        
+        
+        
+        var jsonObj = [String:Any]()
+        
+        jsonObj["id"] = id
+        
+        jsonObj["plan"] = plan
+        
+        jsonObj["user"] = user
+        
+        jsonObj["likesCount"] = likesCount
+        
+        
+        
+        //        jsonObj["myPlans"] = myPlanToJson(myPlans: myPlans)
+        
+        return jsonObj
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    /*   if fromJson["comments"] != nil{
+     
+     let plans  = fromJson["comments"] as! [String: Any]
+     
+     for (_, val) in plans {
+     
+     let temp = val as! [String:Any]
+     
+     let comment = Comment(json: temp)
+     
+     comments.append(comment)
+     
+     }
+     
+     }*/
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
-    
-}
-        
-        
-        
-        
 
-    
+
+
+
 
